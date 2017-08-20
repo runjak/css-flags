56af22fd1a179b8bfac289bab8f4ec7f73095457
Jakob Runge <sicarius@g4t3.de>
2017-08-20 09:40:03 +0200

    refactor Flags to use gradient util

--- src/Flags/Armenia.js
+++ src/Flags/Armenia.js
@@ -1,17 +1,17 @@
 // @flow
 import React from 'react';
+
 import LinearFlag from './LinearFlag';
+import gradient from '../utils/gradient';
 
 const red = '#DA0009';
 const blue = '#0030A1';
 const yellow = '#F3A900';
 
-const gradient = `${red} 0%, ${red} 33%, ${blue} 33%, ${blue} 66%, ${yellow} 66%, ${yellow} 100%`;
-
 export default function Armenia() {
   return (
     <LinearFlag
-      gradient={gradient}
+      gradient={gradient([red, blue, yellow])}
     />
   );
 }
